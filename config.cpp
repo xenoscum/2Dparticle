8bdefebbef982592f90aceec760485c2de1403ed
xenoscum <rsan.1707@gmail.com>
2023-11-14 19:27:42 +0300

    start

---
+++ config.cpp
@@ -0,0 +1,42 @@
+#include <unordered_map>
+#include <vector>
+
+class Model{
+    class Particle;
+    
+    const int N;
+    const double tau;
+    std::vector<Particle> particles;
+
+public:
+    Model(int N_, double tau_): N(N_), tau(tau_) {
+        particles.resize(N);
+    }
+
+    
+};
+
+class Model::Particle{
+    double x;
+    double y;
+    double radius;
+    double density;
+    std::pair<double, double> velocity;
+    int cell;
+    int id;
+
+    inline static int counter = 0;
+
+public:
+    Particle(double x_, double y_, double radius_, double density_, std::pair<double, double> velocity_): id(++counter) {
+        x = x_;
+        y = y_;
+        radius = radius_;
+        density = density_;
+        velocity = velocity_;
+    }
+
+    void move() {
+        x += 1;
+    }
+};
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "sasha_dura\n";
+
+    return 0;
+}
